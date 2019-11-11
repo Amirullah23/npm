dbae28064d3f14e9707dcfb869ebd7acb52afbff
amirullah23 <amirullah.kasang@gmail.com>
2019-11-11 14:15:16 +0700

    add demon

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const chalk = require("chalk")
 
 console.log(chalk.red("hello yarn with color red"))
+console.log(chalk.green("hello yarn with color red"))
+console.log(chalk.white("hello yarn with color red"))
+console.log(chalk.blue("hello yarn with color red"))
+console.log(chalk.red("hello yarn with color red"))
+console.log(chalk.red("hello yarn with color red"))
